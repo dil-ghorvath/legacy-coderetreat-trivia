98b0949ee907f420610a8000a6da12172988fd8b
Gergő Horváth <ghorvath@diligent.com>
2024-12-02 10:18:30 +0100

    Add intital test

---
+++ C#/Trivia/Tests/GameTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Trivia;
+using Xunit;
+
+namespace Tests;
+
+public class GameTests
+{
+    [Fact]
+    public void When_PlayerIsAdded_Console_Message_Should_Appear()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        var sut = CreateSut();
+
+        sut.Add("Chet");
+
+        Assert.Contains("Chet was added", output.ToString());
+    }
+
+    public Game CreateSut()
+    {
+        return new Game();
+    }
+}
