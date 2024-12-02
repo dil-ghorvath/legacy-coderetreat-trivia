2156bd6643b9c15bf4b946490a80fb864397b798
Gergő Horváth <ghorvath@diligent.com>
2024-12-02 11:54:15 +0100

    Initial approval tests

--- C#/Trivia/Tests/ApprovalTests.cs
+++ C#/Trivia/Tests/ApprovalTests.cs
@@ -21,6 +21,59 @@ public class ApprovalTests
         return VerifyXunit.Verifier.Verify(rockQuestion);
     }
 
+    [Fact]
+    public Task AddPlayersApproval()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        var sut = CreateSut();
+
+        sut.Add("Chet");
+
+        return VerifyXunit.Verifier.Verify(output.ToString());
+    }
+
+    [Fact]
+    public Task AddTwoPlayersApproval()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        var sut = CreateSut();
+
+        sut.Add("Chet");
+        sut.Add("Pat");
+
+        return VerifyXunit.Verifier.Verify(output.ToString());
+    }
+
+    [Fact]
+    public Task AddSevenPlayersApproval()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        var sut = CreateSut();
+
+        for (int i = 0; i < 7; i++)
+        {
+            sut.Add("Player" + i);
+        }
+
+        return VerifyXunit.Verifier.Verify(output.ToString());
+    }
+
+    [Fact]
+    public Task RollApproval()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        var sut = CreateSut();
+
+        sut.Add("Chet");
+        sut.Roll(1);
+
+        return VerifyXunit.Verifier.Verify(output.ToString());
+    }
+
     public Game CreateSut()
     {
         return new Game();
