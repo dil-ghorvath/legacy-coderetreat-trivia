225b44ebcee49fcf61a6cdbdd3459db8419f7f3a
Gergő Horváth <ghorvath@diligent.com>
2024-12-02 11:48:36 +0100

    Idk

---
+++ C#/Trivia/Tests/ApprovalTests.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Trivia;
+using Xunit;
+
+namespace Tests;
+
+public class ApprovalTests
+{
+    [Fact]
+    public Task ApprovalTest()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        var sut = CreateSut();
+
+        var rockQuestion = sut.CreateRockQuestion(1);
+
+        return VerifyXunit.Verifier.Verify(rockQuestion);
+    }
+
+    public Game CreateSut()
+    {
+        return new Game();
+    }
+}
